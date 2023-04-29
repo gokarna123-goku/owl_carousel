a3936f5c15f5bcafc4cf49d9d176fc6192763733
gokarna123-goku <agokarna444@gmail.com>
2023-04-29 10:25:37 +0545

    updated the color

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
-// $(document).ready(function () {
-//   $(".owl-carousel").owlCarousel({
-//     loop: true,
-//     margin:10,
-//     responsiveClass:true,
-//     nav: true,
-//     dots: true,
-//     center: true,
-//     responsive: {
-//       0: {
-//         items: 1,
-//       },
-//       600: {
-//         items: 2,
-//       },
-//       1000: {
-//         items: 3,
-//       },
-//     },
-//   });
-// });
-
+// 
 $(".owl-carousel").owlCarousel({
   loop: false,
-  autoplay:false,
+  autoplay: false,
   margin: 20,
   nav: true,
-  dots:false,
-  responsiveClass:true,
+  dots: false,
+  responsiveClass: true,
   responsive: {
     0: {
       items: 1,
